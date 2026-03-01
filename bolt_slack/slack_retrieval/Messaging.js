f7ca6bcd0b9d85f83f5d8226f1a04e5fa5d750bb
Donte Harmon <donteharmon13@gmail.com>
2026-03-01 12:05:31 -0500

    Enabled object posting into database
    - Messaging and retrieval files takes channel history of public channels in a workspace and uploads that data into the database

---
+++ bolt_slack/slack_retrieval/Messaging.js
@@ -0,0 +1,58 @@
+/*
+Retrieves messages from Slack channels and inserts into MongoDB Atlas Database.
+Limited to public channels that the bot is a member of.
+MongoDB handles duplicates automatically.
+*/
+
+const { getConversationHistory, getChannelList } = require('./retrieval');
+const { MongoClient } = require('mongodb');
+require('dotenv').config({ path: '../.env' });
+
+// Import database credentials from .env file
+const DB_USER = process.env.MONGODB_USER;
+const DB_PASSWORD = process.env.MONGODB_PASSWORD;
+
+console.log("DB_USER:", DB_USER);
+console.log("DB_PASSWORD:", DB_PASSWORD);
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@suds-cluster.poxtvnp.mongodb.net/?appName=SUDs-Cluster`;
+const mongoClient = new MongoClient(uri);
+
+// MongoDB connection setup
+async function connectDB(){
+    try{
+        await mongoClient.connect();
+        const db = mongoClient.db("slack");
+        return db;
+    } catch (error) {
+        console.error("Database connection error:", error);
+        throw error;
+    }
+}
+
+
+// Main function to retrieve messages and insert into MongoDB
+async function insertMessagesToDB() {
+    try {
+        const channelList = await getChannelList();
+        const db = await connectDB();
+
+        for (const channel of channelList) {
+            const messages = await getConversationHistory(channel.id);
+            
+            //Skips channels that the bot is not a member of or have no messages
+            if (!messages || messages.length === 0) {
+                console.log(`Skipping #${channel.name} — Bot is not a member or no messages found.`);
+                continue;
+                }
+
+            const collection = db.collection(channel.name);
+            await collection.insertMany(messages);
+        }
+        console.log("All messages inserted into the database.");
+    } catch (error) {
+        console.error("Error inserting messages to DB:", error.message);
+    }
+}
+
+insertMessagesToDB();
---
+++ bolt_slack/slack_retrieval/retrieval.js
@@ -0,0 +1,61 @@
+/*
+This file retrieves messages from channels in the workspace.
+Limited by bot membership and permissions.
+*/
+const { App } = require('@slack/bolt');
+require('dotenv').config({ path: '../.env' });
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+//Retrieves messages from a channel, filters for type, user, text, and timestamp, and returns cleaned messages
+async function getConversationHistory(channelId) {
+  try {
+    console.log(`Attempting to pull messages from: ${channelId}`);
+    
+    const result = await app.client.conversations.history({
+      channel: channelId,
+      // limit = 1000 per call
+    });
+
+    // Filters out messages to the user, type, text, and timestamp
+    const cleanedMessages = result.messages.map(msg => ({
+      user: msg.user,
+      type: msg.type,
+      text: msg.text,
+      ts:   msg.ts
+    }));
+
+    console.log(`Message data successfully retrieved from: ${channelId}`);
+    return cleanedMessages;
+  } catch (error) {
+    const errCode = error.data?.error;
+
+    //Conditional for channels the bot isn't a member of 
+    if (errCode === "not_in_channel") {
+      //console.log(`Skipping ${channelId} — bot is not a member of this channel`);
+      return []; 
+    }
+
+    console.error("Extraction Error:", error.data ? error.data.error : error.message);
+  }
+}
+
+// Retrieves list of channels in the workspace. Only permits for public channels
+async function getChannelList(){
+  try {
+    const channelList = await app.client.conversations.list({
+      types: "public_channel"
+    });
+    console.log("Channel list successfully retrieved!");
+    return channelList.channels;
+  }
+  catch (error) {
+    console.error("Channel List Retrieval Error:", error.data ? error.data.error : error.message);
+  }
+}
+
+
+module.exports = { getConversationHistory, getChannelList };
