733e2a496624d9ee357bdb82bcbd345db215e369
samirbuch <samir.buch@temple.edu>
2026-03-11 16:14:17 -0400

    PartnerSearch: improve UX

--- src/components/PartnerSearch.tsx
+++ src/components/PartnerSearch.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Stack, Text, Button, TextInput, Group } from "@mantine/core";
+import { Stack, Text, Button, TextInput, Group, Divider } from "@mantine/core";
 import { useRouter } from "next/router";
+import { IconArrowRight } from "@tabler/icons-react";
 
 export default function PartnerSearch() {
   const [query, setQuery] = useState("");
@@ -9,11 +10,6 @@ export default function PartnerSearch() {
   const handleRandom = () => {
     // replace with real random partner logic
     console.log("Pick a random partner");
-  };
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      handleSearch(); // Trigger search on Enter key press
-    }
   };
 
   const handleSearch = () => {
@@ -46,6 +42,29 @@ export default function PartnerSearch() {
 
         {/* Right Side: Controls */}
         <Group gap="md" align="center">
+          <form onSubmit={handleSearch}>
+            <Group gap="md" align="center">
+              <TextInput
+                placeholder="Enter Room ID"
+                size="lg"
+                radius="xs"
+                value={query}
+                onChange={(e) => setQuery(e.currentTarget.value)}
+                styles={{ input: { width: 300 } }}
+              />
+              <Button
+                rightSection={<IconArrowRight />}
+                size="lg"
+                type="submit"
+                radius="xs"
+              >
+                Find
+              </Button>
+            </Group>
+          </form>
+
+          <Divider orientation="vertical" />
+
           <Button
             size="lg"
             color="black"
@@ -55,16 +74,6 @@ export default function PartnerSearch() {
           >
             Random
           </Button>
-
-          <TextInput
-            placeholder="Enter Room ID"
-            size="lg"
-            radius="xs"
-            value={query}
-            onChange={(e) => setQuery(e.currentTarget.value)}
-            onKeyDown={handleKeyDown}
-            styles={{ input: { width: 300 } }}
-          />
         </Group>
       </Group>
     </Stack>
