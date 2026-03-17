16cd8d1662ef1deac992960cbb85550d264b3f27
Wyatt Zantua <tuo18351@temple.edu>
2026-03-17 16:38:15 -0400

    Adjust raw message schema to store all fields in DB

--- mongo_storage/models/Message.js
+++ mongo_storage/models/Message.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const { Schema, model } = require("mongoose");
 
-const msgSchema = new Schema({
+const msgSchema = new Schema(
+  {
     user: String,
     type: String,
     text: String,
     ts: String
-});
+  },
+  { strict: false } // Ensures all raw fields are logged
+);
 
 // Function to creates message model for any collection
 const getMessageModel = (collectionName) => {
