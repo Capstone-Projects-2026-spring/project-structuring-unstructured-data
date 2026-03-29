ec44743521f18f34e0582e3e94376c32faaca753
samirbuch <samir.buch@temple.edu>
2026-03-29 12:08:29 -0400

    GameTestCase: create new parameter button (design only)

--- src/components/gameTests/GameTestCase.tsx
+++ src/components/gameTests/GameTestCase.tsx
@@ -1,12 +1,17 @@
-import { Button, Group, Stack, Table, Text } from "@mantine/core";
-import { IconCode, IconPlayerPlay } from "@tabler/icons-react";
-import { TestableCase } from "./GameTestCasesContext";
+import { ParameterPrimitiveType, ParameterType } from "@/lib/ProblemInputOutput";
+import { Button, ComboboxData, Flex, Group, Popover, Select, SelectProps, Stack, Table, Text, TextInput } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconCheck, IconCode, IconHash, IconList, IconListNumbers, IconMatrix, IconPlayerPlay, IconTable, IconTextSize, IconToggleRight } from "@tabler/icons-react";
+import React from "react";
 import { type Socket } from "socket.io-client";
+import { TestableCase } from "./GameTestCasesContext";
 import ParameterInput from "./ParameterInput";
 
 export interface GameTestCaseProps {
   testableCase: TestableCase,
   onTestCaseChange: (test: TestableCase) => void;
+
+  onNewParameter: (parameter: ParameterType) => void;
 
   // because we might want to show these test cases
   // on the results screen
@@ -77,11 +82,7 @@ export default function GameTestCase(props: GameTestCaseProps) {
       </Table>
 
       <Group align="flex-start" gap="sm">
-        <Button
-          rightSection={<IconCode />}
-        >
-          New Parameter
-        </Button>
+        <NewParameterButton />
         <Button
           color="green"
           rightSection={<IconPlayerPlay />}
@@ -90,5 +91,79 @@ export default function GameTestCase(props: GameTestCaseProps) {
         </Button>
       </Group>
     </Stack>
+  );
+}
+
+function NewParameterButton() {
+  const data: ComboboxData = [
+    { value: "string", label: "String" },
+    { value: "number", label: "Number" },
+    { value: "boolean", label: "Boolean" },
+    { value: "array_string", label: "String Array" },
+    { value: "array_number", label: "Number Array" },
+    { value: "array_array_string", label: "2D String Array" },
+    { value: "array_array_number", label: "2D Number Array" },
+  ];
+
+  const iconProps = {
+    stroke: 1.5,
+    color: "currentColor",
+    opacity: 0.8,
+    size: 20
+  };
+  const icons: Record<ParameterPrimitiveType, React.ReactNode> = {
+    string: <IconTextSize {...iconProps} />,
+    number: <IconHash {...iconProps} />,
+    boolean: <IconToggleRight {...iconProps} />,
+    array_string: <IconList {...iconProps} />,
+    array_number: <IconListNumbers {...iconProps} />,
+    array_array_string: <IconTable {...iconProps} />,
+    array_array_number: <IconMatrix {...iconProps} />
+  };
+
+  const renderSelectOption: SelectProps["renderOption"] = ({ option, checked }) => (
+    <Group flex={1} gap="xs">
+      {icons[option.value as ParameterPrimitiveType]}
+      {option.label}
+      {checked && <IconCheck style={{ marginInlineStart: 'auto' }} {...iconProps} />}
+    </Group>
+  );
+
+  const [opened, { close, toggle }] = useDisclosure();
+  return (
+    <Popover opened={opened} onClose={close}>
+      <Popover.Target>
+        <Button
+          rightSection={<IconCode />}
+          onClick={toggle}
+        >
+          New Parameter
+        </Button>
+      </Popover.Target>
+
+      <Popover.Dropdown>
+        <Flex direction={"column"} gap="xs">
+          <TextInput
+            withAsterisk
+            label="Parameter Name"
+          />
+          <Select
+            withAsterisk
+            label="Type"
+            data={data}
+            renderOption={renderSelectOption}
+            comboboxProps={{ withinPortal: false }}
+          />
+
+          <Button
+            mt="sm"
+            size="sm"
+            onClick={close}
+          >
+            Done
+          </Button>
+        </Flex>
+      </Popover.Dropdown>
+    </Popover>
   );
 }
