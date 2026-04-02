bb1694526861710e34e2e62c2cae8d6d158f83c4
ElanReizas <elanreizas@gmail.com>
2026-04-02 13:57:58 -0400

    I wonder what this could be
    hehehehehe sneaky apple

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -119,7 +119,7 @@ export default function Home() {
                 <title>Infra + Auth Test</title>
                 <meta name="description" content="Tests" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/apple.png" />
             </Head>
 
             <div className={styles.page}>
