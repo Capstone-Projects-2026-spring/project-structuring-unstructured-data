8141ec00eeaefd7ac2830c3d80c1d99e6992b4e5
ElanReizas <elanreizas@gmail.com>
2026-04-02 13:57:58 -0400

    I wonder what this could be
    hehehehehe sneaky apple

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en" {...mantineHtmlProps}>
       <Head>
+        <link rel="icon" href="/apple.png"></link>
         <ColorSchemeScript defaultColorScheme="auto" />
       </Head>
       <body>
